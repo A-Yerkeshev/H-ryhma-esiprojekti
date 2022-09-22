d6e870bfd90efc487eb9ad2ef5d6716e7302ae17
Arman Yerkesh <a.yerkesh@gmail.com>
2022-09-22 07:52:29 +0300

    Initial commit

---
+++ lentopeli.py
@@ -0,0 +1,20 @@
+import geopy
+import mysql.connector
+
+airports = []
+km_total = 0
+dist_by_type = {
+  closed: 0,
+  balloonport: 10,
+  heliport: 50,
+  small_airport: 100,
+  medium_airport: 200,
+  large_airport: 500,
+  seaplane_base: 100
+}
+
+def generate_random_location():
+def fetch_available_airports(curr_lat, curr_long):
+def print_available_airports():
+def move(ident):
+def check_if_arrived():
