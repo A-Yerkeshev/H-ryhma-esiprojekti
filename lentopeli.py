4f04c175e0ed23c7393d1cd4ed457f0cfc3acc49
Arman Yerkesh <a.yerkesh@gmail.com>
2022-09-26 11:28:11 +0300

    Build SQL sentence to fetch list of available airports from databse

--- lentopeli.py
+++ lentopeli.py
@@ -1,4 +1,4 @@
-import geopy
+import math
 import mysql.connector
 
 airports = []
@@ -14,7 +14,32 @@ dist_by_type = {
 }
 
 def generate_random_location():
-def fetch_available_airports(curr_lat, curr_long):
+def fetch_available_airports(curr_lat, curr_long, type):
+  # Define flight radius based on airport type
+  radius_km = None
+  if type in dist_by_type:
+    radius_km = dist_by_type[type]
+  else:
+    raise Exception(f"Airport type '{type}' is invalid.")
+
+  # Select all airports within the reach of current location
+  # ((delta x)_to_km)^2 + ((delta y)_to_km)^2 <= R^2
+  # Latitude: 1 deg = 110.574 km
+  # Longitude: 1 deg = 111.320*cos(latitude) km
+  f"""SELECT ident, name, iso_country, latitude_deg, longitude_deg FROM airport
+  WHERE POWER(({curr_lat} + latitude_deg)*110.574, 2) +
+  POWER(({curr_long} + longitude_deg)*111.320*cos(latitude_deg), 2) <= POWER({radius_km}, 2)
+  AND type != 'closed'
+  OR POWER(({curr_lat} + latitude_deg)*110.574, 2) +
+  POWER(({curr_long} - longitude_deg)*111.320*cos(latitude_deg), 2) <= POWER({radius_km}, 2)
+  AND type != 'closed'
+  OR POWER(({curr_lat} - latitude_deg)*110.574, 2) +
+  POWER(({curr_long} + longitude_deg)*111.320*cos(latitude_deg), 2) <= POWER({radius_km}, 2)
+  AND type != 'closed'
+  OR POWER(({curr_lat} - latitude_deg)*110.574, 2) +
+  POWER(({curr_long} - longitude_deg)*111.320*cos(latitude_deg), 2) <= POWER({radius_km}, 2)
+  AND type != 'closed'
+  ORDER by (latitude_deg + longitude_deg) LIMIT 50;"""
 def print_available_airports():
 def move(ident):
 def check_if_arrived():
