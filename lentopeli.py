806e5ad3528547a3f52e552a37db7fb5c92c62d0
Arman Yerkesh <a.yerkesh@gmail.com>
2022-09-26 15:05:09 +0300

    Change SQL sentence

--- lentopeli.py
+++ lentopeli.py
@@ -33,23 +33,15 @@ def fetch_available_airports(curr_lat, curr_long, type):
     raise Exception(f"Airport type '{type}' is invalid.")
 
   # Select all airports within the reach of current location
-  # ((delta x)_to_km)^2 + ((delta y)_to_km)^2 <= R^2
-  # Latitude: 1 deg = 110.574 km
-  # Longitude: 1 deg = 111.320*cos(latitude) km
+  # Distance = 3963.0 * arccos[(sin(lat1) * sin(lat2)) + cos(lat1) * cos(lat2) * cos(long2 – long1)] * 1.609344
+
   f"""SELECT ident, name, iso_country, latitude_deg, longitude_deg FROM airport
-  WHERE POWER(({curr_lat} + latitude_deg)*110.574, 2) +
-  POWER(({curr_long} + longitude_deg)*111.320*cos(latitude_deg), 2) <= POWER({radius_km}, 2)
+  WHERE 3963.0 * acos((sin(RADIANS({curr_lat})) * sin(RADIANS(latitude_deg))) +
+  cos(RADIANS({curr_lat})) * cos(RADIANS(latitude_deg)) *
+  cos(RADIANS(longitude_deg) - RADIANS({curr_long}))) * 1.609344 <= {radius_km}
   AND type != 'closed'
-  OR POWER(({curr_lat} + latitude_deg)*110.574, 2) +
-  POWER(({curr_long} - longitude_deg)*111.320*cos(latitude_deg), 2) <= POWER({radius_km}, 2)
-  AND type != 'closed'
-  OR POWER(({curr_lat} - latitude_deg)*110.574, 2) +
-  POWER(({curr_long} + longitude_deg)*111.320*cos(latitude_deg), 2) <= POWER({radius_km}, 2)
-  AND type != 'closed'
-  OR POWER(({curr_lat} - latitude_deg)*110.574, 2) +
-  POWER(({curr_long} - longitude_deg)*111.320*cos(latitude_deg), 2) <= POWER({radius_km}, 2)
-  AND type != 'closed'
-  ORDER by (latitude_deg + longitude_deg) LIMIT 50;"""
+  ORDER by (latitude_deg + longitude_deg) LIMIT 50;
+  """
 def print_available_airports():
   print
 def move(ident):
@@ -72,17 +64,6 @@ def check_if_arrived():
       "long":long,
     }
     return parsed
-
-# def fetch_available_airports(curr_lat, curr_long):
-
-
-# def print_available_airports():
-
-
-# def move(ident):
-
-
-# def check_if_arrived():
 
 current = destination = generate_random_location()
 print(current)
