6e909c8ca2e74ab1af98979024d31a4a5b9727c4
metroko <ganondoor@outlook.com>
2022-10-04 21:47:31 +0300

    formatting, tweaked type values a bit, fixed dist calculation  so it doesn't become negative

--- lentopeli.py
+++ lentopeli.py
@@ -3,13 +3,12 @@ import mysql.connector
 import math
 import time
 
-
 yhteys = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
     database='flight_game',
     user='root',
-    password='Cassandra-580',
+    password='toast',
     autocommit=False
 )
 
@@ -19,11 +18,10 @@ temp_dest = ""
 turns_total = 0
 km_total = 0
 dist_by_type = {
-    "closed": 0,
-    "balloonport": 10,
-    "heliport": 50,
-    "small_airport": 200,
-    "medium_airport": 400,
+    "balloonport": 50,
+    "heliport": 100,
+    "small_airport": 300,
+    "medium_airport": 500,
     "large_airport": 1000,
     "seaplane_base": 100
 }
@@ -33,6 +31,7 @@ def get_distance(curr_lat, curr_long, dest_lat, dest_long):
     distance_result = distance.distance([curr_lat, curr_long],
                                         [dest_lat, dest_long]).km
     return distance_result
+
 
 def generate_random_location():
     sql = "SELECT ident, airport.name as airport_name," \
@@ -46,6 +45,7 @@ def generate_random_location():
 
     return tuple_to_dict(result[0])
 
+
 def tuple_to_dict(tuple):
     ident, airport_name, country_name, type, lat, long = tuple
 
@@ -58,13 +58,14 @@ def tuple_to_dict(tuple):
         "long": long,
     }
 
-def fetch_available_airports(curr_lat, curr_long, dest_lat, dest_long, type):
+
+def fetch_available_airports(curr_lat, curr_long, type):
     # Define flight radius based on airport type
     radius_km = None
     if type in dist_by_type:
-      radius_km = dist_by_type[type]
+        radius_km = dist_by_type[type]
     else:
-      raise Exception(f"Airport type '{type}' is invalid.")
+        raise Exception(f"Airport type '{type}' is invalid.")
 
     # Select all airports within the reach of current location,
     # based on airport type, order by ones closest to destination
@@ -88,9 +89,10 @@ def fetch_available_airports(curr_lat, curr_long, dest_lat, dest_long, type):
         airport = tuple_to_dict(entry)
 
         # Calculate in which direction airport is located
-        x = math.cos(airport['lat'])*math.sin(airport['long']-curr['long'])
-        y = math.cos(curr['lat'])*math.sin(airport['lat'])-math.sin(curr['lat'])*math.cos(airport['lat'])*math.cos(airport['long']-curr['long'])
-        bearing = math.degrees(math.atan2(x,y))
+        x = math.cos(airport['lat']) * math.sin(airport['long'] - curr['long'])
+        y = math.cos(curr['lat']) * math.sin(airport['lat']) - math.sin(curr['lat']) * math.cos(
+        airport['lat']) * math.cos(airport['long'] - curr['long'])
+        bearing = math.degrees(math.atan2(x, y))
 
         if bearing >= -22.5 and bearing < 22.5:
             airport['direction'] = 'North'
@@ -123,11 +125,13 @@ def fetch_available_airports(curr_lat, curr_long, dest_lat, dest_long, type):
         for airport in group:
             airports.append(airport)
 
+
 def print_available_airports():
     for i, airport in enumerate(airports):
-        print(f"{str(i + 1)}: {airport['airport_name']}, in {airport['country_name']} - "
-              f"{get_distance(curr['lat'], curr['long'], airport['lat'], airport['long']):.1f} km away"
+        print(f"{str(i + 1)}: {airport['airport_name']}, {airport['type']} in {airport['country_name']} - "
+              f"{get_distance(curr['lat'], curr['long'], airport['lat'], airport['long']):.1f} km"
               f" to the {airport['direction']}.")
+
 
 # initialize start and end locations, calculate distance
 check_if_arrived = False
@@ -135,31 +139,33 @@ curr = generate_random_location()
 dest = generate_random_location()
 dist = get_distance(curr["lat"], curr["long"], dest["lat"], dest["long"])
 
-while dest == curr or (dist > 6000 or dist < 3000):
+while dest == curr or (dist > 4000 or dist < 1500):
     dest = generate_random_location()
     dist = get_distance(curr["lat"], curr["long"], dest["lat"], dest["long"])
 
 # Start the game
 while curr['ident'] != dest['ident']:
     print(f"\nYour current location is '{curr['airport_name']}', {curr['type']} in {curr['country_name']}"
-    f"\nYour destination is '{dest['airport_name']}' in {dest['country_name']}."
-    f"\nThe destination is {dist:.0f} km away.")
+          f"\nYour destination is '{dest['airport_name']}' in {dest['country_name']}."
+          f"\nThe destination is {dist:.0f} km away.")
 
     input("\nPress 'Enter' to fetch the nearest airports.")
-    fetch_available_airports(curr["lat"], curr["long"], dest["lat"], dest["long"], curr["type"])
+    fetch_available_airports(curr["lat"], curr["long"], curr["type"])
     print_available_airports()
 
-    index = int(input("\nEnter the index of the airport you want to go to: ")) - 1
+    index = int(input("\nEnter the index of the airport you want to fly to: ")) - 1
 
     while index >= len(airports) or index < 0:
         print(f"Your input is invalid. Please type a number between 0 and {len(airports)}")
-        index = int(input("\nEnter the index of the airport you want to go to: ")) - 1
+        index = int(input("\nEnter the index of the airport you want to fly to: ")) - 1
 
     temp_dest = airports[index]
     flight = get_distance(curr['lat'], curr['long'], temp_dest['lat'], temp_dest['long'])
     flight_compare = get_distance(temp_dest['lat'], temp_dest['long'], dest['lat'], dest['long'])
     if dist > flight_compare:
         dist = dist - flight
+        if dist < 0:
+            dist = dist * -1
     else:
         dist = dist + flight
     turns_total = turns_total + 1
@@ -167,13 +173,13 @@ while curr['ident'] != dest['ident']:
     curr = airports[index]
     print(f"\nYou fly {flight:.1f} km to {curr['airport_name']}.")
     print("\r>", end="")
-    time.sleep(0.3)
+    time.sleep(0.2)
     print("\r----->", end="")
-    time.sleep(0.3)
+    time.sleep(0.2)
     print("\r---------->", end="")
-    time.sleep(0.3)
+    time.sleep(0.2)
     print("\r      --------->", end="")
-    time.sleep(0.3)
+    time.sleep(0.2)
     print("\r               >", end="")
     time.sleep(0.3)
 print(f"\nCongratulations! You made it to your destination, {dest['airport_name']}.\n"
